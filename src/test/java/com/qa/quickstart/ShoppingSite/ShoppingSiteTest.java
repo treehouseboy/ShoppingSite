848ad50660523cd88edd4ef46512dadcd7d2d1b4
brassrabbit <george.hardy@hotmail.co.uk>
2018-05-21 17:49:10 +0100

    Add contents

---
+++ src/test/java/com/qa/quickstart/ShoppingSite/ShoppingSiteTest.java
@@ -0,0 +1,43 @@
+package com.qa.quickstart.ShoppingSite;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ShoppingSiteTest {
+
+	WebDriver driver;
+	WebElement checkElement;
+	String username = "person";
+	String password = "password";
+
+	@Before
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\WebDev\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void dressTest() {
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com/index.php");
+		driver.findElement(By.id("search_query_top")).sendKeys("dress");
+		driver.findElement(By.name("submit_search")).click();
+		checkElement = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a"));
+		assertEquals("Printed Summer Dress", checkElement.getText());
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+
+}
